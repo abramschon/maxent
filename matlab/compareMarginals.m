c73c0565a89e896d974962129ece8e520d346c5c
Abram Schonfeldt <abram.schonfeldt@gmail.com>
2021-12-26 13:14:30 +0100

    Small tweaks to range

--- matlab/compareMarginals.m
+++ matlab/compareMarginals.m
@@ -1,5 +1,5 @@
 %% Getting 3rd order correlations from indep model
-NN = 50;
+NN = 20;
 indep = maxent.createModel(NN,'indep');
 indep = maxent.trainModel(indep, randi([0,1], [NN,10000]) );
 disp("Analytic 3 corrs")
@@ -20,53 +20,55 @@ load ../data/shuffled_data/data2014
 [total_N, obvs] = size(train_reps);
 
 % parameters
-NN = 100;
-shuffle = 'stimulus'; % 'stimulus' or 'time'
-prefix = "../data/trained_models/" + shuffle + "_" + NN;
-n_reps = 10;
-n_samples = NN*10000;
+for NN = 10:25
+    % NN = 100;
+    shuffle = 'stimulus'; % 'stimulus' or 'time'
+    prefix = "../data/trained_models/" + shuffle + "_" + NN;
+    n_reps = 10;
+    n_samples = NN*10000;
 
-% to save pk
-pks = zeros(NN+1, n_reps);
-ind_pks = zeros(NN+1, n_reps);
-ising_pks = zeros(NN+1, n_reps);
+    % to save pk
+    pks = zeros(NN+1, n_reps);
+    ind_pks = zeros(NN+1, n_reps);
+    ising_pks = zeros(NN+1, n_reps);
 
 
-for rep = 1:n_reps
-    disp(rep)
-    rng(rep)
-    id_N = randperm(total_N, NN); 
-    X = train_reps(id_N,:); % select the training data
-    % get data correlations
-    pks(:,rep) = samplepk(X);
-    
-    % get model correlations
-    
-    %indep
-    m_ind = load(prefix + "_indep_" + rep);
-    disp("Indep sampling")
-    samples = maxent.generateSamples(m_ind.model, n_samples); % returns a N x n_samples matrix
-    disp("Indep pk")
-    ind_pks(:,rep) = samplepk(samples);
-    
-    %pairwise
-    m_ising = load(prefix + "_pairwise_" + rep);
-    disp("Pairwise sampling")
-    samples = maxent.generateSamples(m_ising.model, n_samples); % returns a N x n_samples matrix
-    disp("Pairwise pk")
-    ising_pks(:,rep) = samplepk(samples);
+    for rep = 1:n_reps
+        disp(rep)
+        rng(rep)
+        id_N = randperm(total_N, NN); 
+        X = train_reps(id_N,:); % select the training data
+        % get data correlations
+        pks(:,rep) = samplepk(X);
+
+        % get model correlations
+
+        %indep
+        m_ind = load(prefix + "_indep_" + rep);
+        disp("Indep sampling")
+        samples = maxent.generateSamples(m_ind.model, n_samples); % returns a N x n_samples matrix
+        disp("Indep pk")
+        ind_pks(:,rep) = samplepk(samples);
+
+        %pairwise
+        m_ising = load(prefix + "_pairwise_" + rep);
+        disp("Pairwise sampling")
+        samples = maxent.generateSamples(m_ising.model, n_samples); % returns a N x n_samples matrix
+        disp("Pairwise pk")
+        ising_pks(:,rep) = samplepk(samples);
+    end
+
+    save_prefix = "../results/correlations/" + shuffle + "_" + NN;
+
+    % save in matlab format
+    save(save_prefix + "_indep_pwise_pks_" + n_reps, ...
+        'pks', 'ind_pks', 'ising_pks')
+
+    % save in .csv format
+    writematrix(pks, save_prefix +'_pks.csv'); % save model weights 
+    writematrix(ind_pks, save_prefix +'_ind_pks.csv'); % save model weights 
+    writematrix(ising_pks, save_prefix +'_ising_pks.csv'); % save model weights 
 end
-
-save_prefix = "../results/correlations/" + shuffle + "_" + NN;
-
-% save in matlab format
-save(save_prefix + "_indep_pwise_pks_" + n_reps, ...
-    'pks', 'ind_pks', 'ising_pks')
-
-% save in .csv format
-writematrix(pks, save_prefix +'_pks.csv'); % save model weights 
-writematrix(ind_pks, save_prefix +'_ind_pks.csv'); % save model weights 
-writematrix(ising_pks, save_prefix +'_ising_pks.csv'); % save model weights 
 
 %% 3-wise correlations
 clear
