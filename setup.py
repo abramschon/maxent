71137125753642d9794f6dad7c6df3584ce4a580
Bram <bram.schon@gmail.com>
2022-01-30 13:16:16 +0000

    Doesn't require all requirements to be installed

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires=requirements,
+    #install_requires=requirements,
     python_requires=">=3.8",
 )
